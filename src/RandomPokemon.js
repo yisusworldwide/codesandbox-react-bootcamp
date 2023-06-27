45195dc97c0a008b89237dd69b7b1d4ac80d79ad
Jesus Jimenez <84227371+yisusworldwide@users.noreply.github.com>
2023-06-27 11:04:56 +0000

    Update RandomPokemon.js

--- src/RandomPokemon.js
+++ src/RandomPokemon.js
@@ -1,9 +1,11 @@
-export default function (RandomPokemon) {
+export default function RandomPokemon() {
   const num = Math.floor(Math.random() * 151) + 1;
-
+  const url = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${num}.png
+`
   return (
     <section>
       <h2>Pokemon #{num}</h2>
+      <img src={url} alt=""/>
     </section>
   );
 }
